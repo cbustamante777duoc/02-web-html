f35506f5b335e2bd018e9c6201b0f4fc727d9ecc
Cristian Bustamante <cra.bustamante@alumnos.duoc.cl>
2022-03-05 13:19:32 -0300

    vue pagina

---
+++ index.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+
+    setup() {
+        const title = 'Auto';
+        const power = 80;
+
+        return{
+            title,
+            power
+        };
+    },
+
+});
